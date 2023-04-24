783bd3e5e143ecf35e7230ee7a803d3a4347f95f
variousnguyen1802 <phongphunguyen7575a@gmail.com>
2023-04-25 05:31:51 +0700

    TS-3: Create Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './UserModule/user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
